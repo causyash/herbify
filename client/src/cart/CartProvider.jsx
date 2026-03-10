5b4fa5281184cb576d409fe136a7b512d44b0e22
Yash <yashpatel7102005@icloud.com>
2026-03-10 16:58:55 +0530

    fix(lint): clean up linting errors for vercel build

--- client/src/cart/CartProvider.jsx
+++ client/src/cart/CartProvider.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-refresh/only-export-components */
 import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import { useAuth } from '../auth/AuthProvider.jsx'
 import { api } from '../lib/api'
